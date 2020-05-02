52e2060cf0c900b3ec64b1804f1463426635a519
Raghav Jajodia <raghavjajodia@gmail.com>
2020-05-01 20:28:45 -0400

    added the new code for lm

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ import math
 
 
 class LM_latent(nn.Module):
-    def __init__(self,vocab_size,hidden_size,token_embedding_size,tag_embedding_size,tags,mask,device, lstm_layers=1):
+    def __init__(self, vocab_size, tag_wise_vocab_size, hidden_size, token_embedding_size, tag_embedding_size, lstm_layers=1):
 
         super(LM_latent, self).__init__()
 
@@ -14,18 +14,14 @@ class LM_latent(nn.Module):
         self.hidden_size = hidden_size
         self.token_embedding_size = token_embedding_size
         self.tag_embedding_size = tag_embedding_size
-        self.num_tags = len(tags)+1
-        self.tags = tags
-        self.mask = mask
-        self.device=device
+        self.num_tags = len(tag_wise_vocab_size)
+        self.tag_wise_vocabsize = tag_wise_vocab_size
         
-        self.token_embedding = nn.Embedding(self.vocab_size,self.token_embedding_size)
+        self.token_embedding = nn.Embedding(self.vocab_size, self.token_embedding_size)
         self.lstm = nn.LSTM(self.token_embedding_size, self.hidden_size, num_layers = lstm_layers, batch_first = True)
-        self.tag_linear = nn.Linear(self.tag_embedding_size,self.num_tags)
-        self.lower_hidden = nn.Linear(self.hidden_size,self.tag_embedding_size)
-        self.tag_projections = nn.ModuleList([nn.Linear(self.hidden_size,self.vocab_size) for i in range(self.num_tags)])
-        
-        
+        self.tag_linear = nn.Linear(self.tag_embedding_size, self.num_tags)
+        self.lower_hidden = nn.Linear(self.hidden_size, self.tag_embedding_size)
+        self.tag_projections = nn.ModuleList([nn.Linear(self.hidden_size, self.tag_wise_vocabsize[i]) for i in range(self.num_tags)])
         
     def forward(self,input_seq):
         
@@ -37,6 +33,5 @@ class LM_latent(nn.Module):
         h_lower = self.lower_hidden(h) #batch_size , sent_len, 100
         tag_logits = self.tag_linear(h_lower) #batch_size, sent_len, num_tags
         
-        word_distributions_logits = torch.stack([self.tag_projections[i](h) for i in range(self.num_tags)],dim=2) #batch_size, sent_len, num_tags, vocab_size
-        
+        word_distribution_logits = [self.tag_projections[i](h) for i in range(self.num_tags)] #for ith tag -> batch_size, sent_len, i_vocab_size       
         return tag_logits, word_distribution_logits
